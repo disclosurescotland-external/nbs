dc3c32b7056ae2bb22bdb2128de59f4d72d6f537
KCarr02 <kath.carrick@disclosurescotland.gov.scot>
2025-09-02 08:58:40 +0100

    added notes and dashboard page

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,6 @@
 module.exports = {
 
-  // Insert values here
+  // Insert values here - ANYTHING THAT YOU GIVE A name YOU CAN SET HERE AND THEN PLAY IT BACK!!!
+  // provide default values for user session data. They will only be added if the data doesnt already exist.
 
 }
